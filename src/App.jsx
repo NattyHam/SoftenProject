ec40e6d8048cdbf05f097b2066e72b8c8c4456a3
NattyHam <pnbookclub@gmail.com>
2024-10-16 01:18:44 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Navbar />
+      ซอฟเอนหัวควยยยยยยยยยยยยยยยยยย
+      <LeftBar />
+      อ้าาาาาาาาาาาา
+      <RightBar />
+      กรี๊ดดดดดดดดดดดดดดดด
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">Navbarอ้ากแม่เย้ดดดดด</div>
+    )
+}
+
+export default Navbar
